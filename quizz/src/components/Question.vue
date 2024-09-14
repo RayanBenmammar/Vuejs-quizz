4e733e75c4b22a355d8bc3c542cb0188c461a79d
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-09-14 15:42:01 +0200

    handle add answer

--- quizz/src/components/Question.vue
+++ quizz/src/components/Question.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { computed, ref } from "vue";
 const props = defineProps({
   question: Object,
 });
+const answer = ref(null);
+const emits = defineEmits(["answer"]);
+const hasAnswer = computed(() => !!answer.value);
 </script>
 
 <template>
@@ -10,16 +14,30 @@ const props = defineProps({
     <ul>
       <li v-for="(choice, index) in question.choices" :key="choice">
         <label :for="`answer${index}`">
-          <input type="radio" name="answer" :id="`answer${index}`" />
+          <input
+            type="radio"
+            name="answer"
+            :id="`answer${index}`"
+            :value="choice"
+            v-model="answer"
+          />
           {{ choice }}
         </label>
       </li>
     </ul>
+    <button :disabled="!hasAnswer" @click="emits('answer', answer)">
+      Question suivante
+    </button>
   </div>
 </template>
 
 <style scoped>
 .question {
   padding-top: 2rem;
+}
+
+.question button {
+  margin-left: auto;
+  display: block;
 }
 </style>
--- quizz/src/components/Quizz.vue
+++ quizz/src/components/Quizz.vue
@@ -8,12 +8,18 @@ const props = defineProps({
 
 const step = ref(0);
 const question = computed(() => props.quizz.questions[step.value]);
+const answers = ref(props.quizz.questions.map(() => null));
+
+const addAnswer = (answer) => {
+  answers.value[step.value] = answer;
+  step.value++;
+};
 </script>
 
 <template>
   <div>
     <h1>{{ quizz.title }}</h1>
     <Progress :value="step" :max="quizz.questions.length - 1" />
-    <Question :question="question" v-if="question" />
+    <Question :question="question" @answer="addAnswer" v-if="question" />
   </div>
 </template>
