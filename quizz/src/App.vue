27ee09d2c57f1366e0614f470256796f150736d1
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-09-14 14:46:23 +0200

    fetch data from json + add pico css

--- quizz/src/App.vue
+++ quizz/src/App.vue
@@ -1,7 +1,33 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const quizz = ref(null);
+const state = ref("loading");
+
+onMounted(() => {
+  fetch("/quizz.json")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch quiz data");
+      }
+    })
+    .then((data) => {
+      quizz.value = data;
+      state.value = "loaded";
+    })
+    .catch((error) => {
+      state.value = "error";
+    });
+});
+</script>
 
 <template>
-  <h1>Hello World</h1>
+  <div v-if="state === 'error'">
+    <p>Impossible de charger le questionnaire</p>
+  </div>
+  <div :aria-busy="state === 'loading'">{{ quizz }}</div>
 </template>
 
 <style scoped></style>
